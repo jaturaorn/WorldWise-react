3681c80c52d850c15c839736e3444465b6c048b6
jaturaporn <jaturapornff@gmail.com>
2024-01-24 22:33:56 +0700

    first commit & build landing page

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../component/PageNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>AppLayout</p>
+    </div>
+  );
+}
+
+export default AppLayout;
